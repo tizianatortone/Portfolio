7728d88d00cdf2ee0fe06335c62485076c52aa9b
tizianatortone <tiziana_tortone@hotmail.com>
2021-07-03 18:34:49 +0200

    js animation navbar added

---
+++ src/script.js
@@ -0,0 +1,25 @@
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  let windowPosition = window.scrollY > 0;
+  header.classList.toggle("scrolling-active", windowPosition);
+});
+
+let didScroll = false;
+let paralaxTitles = document.querySelectorAll(".paralax-title");
+
+const scrollInProgress = () => {
+  didScroll = true;
+};
+
+const raf = () => {
+  if (didScroll) {
+    paralaxTitles.forEach((element, index) => {
+      element.style.transform = "translateX(" + window.scrollY / 10 + "%)";
+    });
+    didScroll = false;
+  }
+  requestAnimationFrame(raf);
+};
+
+requestAnimationFrame(raf);
+window.addEventListener("scroll", scrollInProgress);
